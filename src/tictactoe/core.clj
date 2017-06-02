bfc3b8c6c746581b1d635e35d2f05a43a16813af
dark-bytes <sudhanshu.singh@go-jek.com>
2017-06-02 13:05:44 +0530

    Implement mini max algorithm

--- src/tictactoe/core.clj
+++ src/tictactoe/core.clj
@@ -2,7 +2,7 @@
   (:gen-class))
 
 (def winning-positions [[0 1 2] [3 4 5] [6 7 8] [0 4 8] [2 4 6] [0 3 6] [1 4 7] [2 5 8]])
-(def board [[" " " " " "] [" " " " " "] [" " " " " "]])
+(def board [["O" " " "O"] ["X" "X" "O"] [" " " " " "]])
 
 (defn print-row
   [row]
@@ -20,7 +20,7 @@
          (<= y 2)]}
   ((board x) y))
 
-(get-cell board 0 0)
+(get-cell board 2 2)
 
 (defn is-empty
   [board x y]
@@ -61,6 +61,74 @@
         ((println temp-board) (move temp-board))))
     board))
 
+(defn opponent
+  [player]
+  (if (= player "O")
+    "X"
+    "O"))
+
+(opponent "X")
+
+ ;; (defn mini-max
+ ;;  [board player-value]
+ ;;  (if (not= (has-empty-cells? board) nil)
+ ;;    (map #(mini-max % (opponent player-value)) (for [x (range 3)]
+ ;;           (for [y (range 3)](println (mark-cell board x y player-value)))))))
+
+(defn draw?
+  [board]
+  (and (= (has-empty-cells? board) nil) (or  (= (win? (flatten board) "X") nil)) (= (win? (flatten board) "O") nil)))
+
+(draw? board)
+
+(defn game-over?
+  [board]
+  (or (win? (flatten board) "X") (win? (flatten board) "O") (= (has-empty-cells? board) nil)))
+
+(game-over? board)
+
+(defn calculate-score
+  [board player-value depth]
+  (if (draw? board)
+    0
+    (let [isWinner (win? (flatten board) player-value)]
+      (if (= isWinner true)
+        (- 10 depth)
+        (- depth 10)))))
+
+(calculate-score board "O" 2)
+
+(quot 7 3)
+(rem 7 3)
+
+(def board-2 [["O" " " "O"] ["X" "X" "O"] [" " " " "X"]])
+
+
+(defn max-scores
+  [board player-value player-chance depth]
+  (if (game-over? board)
+    (calculate-score board player-value depth)
+    (let [spots (available-spots board)]
+      (apply max (map #(max-scores (mark-cell board (quot % 3) (rem % 3) player-chance) player-value (opponent player-chance) (inc depth)) spots)))))
+
+(max-scores board-2 "X" "O" 0)
+
+(defn mini-max
+  [board player-value]
+  (if (= (has-empty-cells? board) true)
+    (let [spots (available-spots board)
+          scores (map #(max-scores (mark-cell board (quot % 3) (rem % 3) player-value) player-value (opponent player-value) 0) spots)]
+      1))
+
+(mini-max board-2 "X")
+
+(defn available-spots
+   [board]
+  (map first (filter #(= " " (second %)) (map-indexed vector (flatten board)))))
+
+(available-spots board)
+
+;(min-max board 0 true)
 ;(move board)
 
 (defn -main
